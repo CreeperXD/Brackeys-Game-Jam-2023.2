9d9ac2e0ead1ac7ff10b4ceb8b94d94f6114c21b
CreeperXD <48917115+CreeperXD@users.noreply.github.com>
2023-08-21 02:03:33 +0800

    Add files via upload

---
+++ Player.gd
@@ -0,0 +1,43 @@
+extends CharacterBody2D
+
+@export var diving_speed = 300
+@export var deceleration = -150
+
+'''
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+'''
+
+func _physics_process(delta):
+	'''
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	'''
+	
+	var direction = Vector2.ZERO
+	
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_up"):
+		direction.y -= 1
+	if Input.is_action_pressed("move_down"):
+		direction.y += 1
+	
+	if direction != Vector2.ZERO:
+		direction = direction.normalized()
+		velocity.x = direction.x * diving_speed
+		velocity.y = direction.y * diving_speed
+	else:
+		if velocity.x > 0:
+			velocity.x += deceleration * delta
+		if velocity.x < 0:
+			velocity.x -= deceleration * delta
+		if velocity.y > 0:
+			velocity.y += deceleration * delta
+		if velocity.y < 0:
+			velocity.y -= deceleration * delta
+	
+	move_and_slide()
